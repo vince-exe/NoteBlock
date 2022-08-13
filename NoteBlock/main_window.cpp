2db9a5e6ff5342d8480636e2dde9e94cb02f9326
Vincenzo Caliendo <82732029+vince-exe@users.noreply.github.com>
2022-08-13 19:33:30 +0200

    first update
    first update of the project

---
+++ NoteBlock/main_window.cpp
@@ -0,0 +1,16 @@
+#include "main_window.h"
+#include "ui_main_window.h"
+
+#include <QMenuBar>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
